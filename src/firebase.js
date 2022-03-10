5554ebfb17c7f6032d4435b815975e5c260b2d99
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-03-10 21:48:47 +0530

    added env for firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'AUTH_DOMAIN',
-  projectId: 'PROJECT_ID',
-  storageBucket: 'STORAGE_BUCKET',
-  messagingSenderId: 'MESSAGE_SENDER_ID',
-  appId: 'APP_ID',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // init firebase
