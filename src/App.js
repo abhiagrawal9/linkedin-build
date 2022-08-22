2e021e43c110d90d71a0060cc57c092a74d4eec7
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-08-22 17:49:29 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -20,6 +20,15 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    console.log(process.env.REACT_APP_API_KEY);
+    console.log(process.env.REACT_APP_AUTH_DOMAIN);
+    console.log(process.env.REACT_APP_DATABASE_URL);
+    console.log(process.env.REACT_APP_PROJECT_ID);
+    console.log(process.env.REACT_APP_STORAGE_BUCKET);
+    console.log(process.env.REACT_APP_MESSAGING_SENDER_ID);
+    console.log(process.env.REACT_APP_APP_ID);
+    console.log(process.env.REACT_APP_MEASUREMENT_ID);
+
     const unsubAuth = onAuthStateChanged(auth, (user) => {
       if (!user) {
         dispatch(LogoutAction());
