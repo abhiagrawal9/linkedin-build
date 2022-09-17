93c669c5871111e24d3c589550604d7f50d24d24
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-09-17 21:38:50 +0530

    changes in header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -27,32 +27,38 @@ const Header = () => {
 
   return (
     <header className='header'>
-      <div className='header__left'>
-        <img
-          className='header__icon'
-          src={headerIcon}
-          alt='Linkedin header icon'
-        />
-        <div className='header__search'>
-          <SearchIcon />
-          <input
-            autoComplete='off'
-            type='text'
-            name='search'
-            id='search'
-            placeholder='Search'
+      <div className='header__content'>
+        <div className='header__left'>
+          <img
+            className='header__icon'
+            src={headerIcon}
+            alt='Linkedin header icon'
           />
+          <div className='header__search'>
+            <SearchIcon />
+            <input
+              autoComplete='off'
+              type='text'
+              name='search'
+              id='search'
+              placeholder='Search'
+            />
+          </div>
         </div>
-      </div>
-      <div className='header__right'>
-        <HeaderOption Icon={HomeIcon} title='Home' />
-        <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
-        <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
-        <HeaderOption Icon={ChatIcon} title='Messaging' />
-        <HeaderOption Icon={NotificationsIcon} title='Notifications' />
-        {user && (
-          <HeaderOption avatar={true} onClick={logoutHandler} title='Logout' />
-        )}
+        <div className='header__right'>
+          <HeaderOption Icon={HomeIcon} title='Home' />
+          <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+          <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+          <HeaderOption Icon={ChatIcon} title='Messaging' />
+          <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+          {user && (
+            <HeaderOption
+              avatar={true}
+              onClick={logoutHandler}
+              title='Logout'
+            />
+          )}
+        </div>
       </div>
     </header>
   );
