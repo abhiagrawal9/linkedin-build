cf718dfc20a5a770058cb488f459968b85c514ad
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-09-17 16:17:18 +0530

    added types for widgets

--- src/components/Widgets/Widgets.tsx
+++ src/components/Widgets/Widgets.tsx
@@ -4,7 +4,7 @@ import InfoIcon from '@material-ui/icons/Info';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
 const Widgets = () => {
-  const newsArticle = (heading, subTitle) => {
+  const newsArticle = (heading: string, subTitle: string) => {
     return (
       <div className='widgets__article'>
         <div className='widgets__articleLeft'>
